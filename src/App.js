47b2b1ae76ca8dd8d66a1631eab2e71f31c84bfc
PhilipPhil <philipgarabandic@gmail.com>
2022-11-24 15:45:02 -0800

    got it running

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
-import './App.css';
+import ReactStickyNotes from './components/react-sticky-notes';
+import './app.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-body">
+      <ReactStickyNotes
+        backgroundColor="#fefefe"
+        useCSS={true}
+        containerHeight={"400px"}
+      />
     </div>
+
   );
 }
 
